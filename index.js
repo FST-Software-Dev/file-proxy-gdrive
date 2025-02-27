5da3b7e6335905e0b16daf737589c000dcd3c35c
muammarzaki <muammarzaki2004@gmail.com>
2025-02-27 16:49:35 +0700

    fix: the request param at end fetch-pdf endpoint

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 
-app.get("/fetch-pdf/hello/:test", async (req, res) => {
-    const fileUrl = `https://drive.google.com/uc?export=download&id=${req.params.test}`;
+app.get("/fetch-pdf/:id", async (req, res) => {
+    const fileUrl = `https://drive.google.com/uc?export=download&id=${req.params.id}`;
     const response = await axios.get(fileUrl, {responseType: "arraybuffer"});
     res.setHeader("Content-Type", "application/pdf");
     res.send(response.data);
